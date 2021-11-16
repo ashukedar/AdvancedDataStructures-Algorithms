0ebd878791deb16997d22bf5c6ef15a635600a57
Ashutosh Kedar <ashutosh10.kedar@gmail.com>
2021-11-16 13:16:36 +0530

    Corrected code for incorrect order

--- BTrees/main.py
+++ BTrees/main.py
@@ -1,3 +1,4 @@
+import math
 import bisect
 from tree import Tree
 
@@ -13,22 +14,22 @@ def getIntGreaterThan1(inputText):
             print("Invalid Input. Expected inpur: Integer greater than 0")
 
 minDegree = getIntGreaterThan1("Enter the min degree for BTree: ")
-tree = Tree(minDegree)
+tree = Tree(math.ceil(minDegree/2))
 
 while True:
     print("1. Insert Item\n2. Delete Item")
     print("3. Display Tree\n4. Exit")
-    #try:
-    choice = int(input("Enter choice: "))
-    if(choice == 1):
-        tree.insert(float(input("Enter the value to be inserted: ")))
-    elif(choice == 2):
-        tree.remove(float(input("Enter the value to be deleted: ")))
-    elif(choice == 3):
-        tree.traverse()
-    elif(choice == 4):
-        break
-    else:
-        raise Exception()
-    #except:
-       #print("Invalid Input.\n");
+    try:
+        choice = int(input("Enter choice: "))
+        if(choice == 1):
+            tree.insert(float(input("Enter the value to be inserted: ")))
+        elif(choice == 2):
+            tree.remove(float(input("Enter the value to be deleted: ")))
+        elif(choice == 3):
+            tree.traverse()
+        elif(choice == 4):
+            break
+        else:
+            raise Exception()
+    except:
+        print("Invalid Input.\n");
